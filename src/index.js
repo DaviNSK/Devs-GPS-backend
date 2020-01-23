8209bdca170efd9a00051f204f71fc4ffcf167e2
user.name <davicardoso.2103@gmial.com>
2020-01-23 14:10:01 -0200

    Devs GPS, backend

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server); 
 
 mongoose.connect('mongodb+srv://davic:davipt2020@cluster0-hhiuw.mongodb.net/week10?retryWrites=true&w=majority', {
 useNewUrlParser: true, 
 useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-app.listen(2123);
+server.listen(2123);
